368a2a087038d69b8df17ad2c67a810f326c8776
Erik Borgeteien Hansen <github@borgeteienhansen.no>
2020-09-22 14:06:26 +0200

    ghkølfd

--- main.py
+++ main.py
@@ -16,29 +16,36 @@ color_sensor = ColorSensor(Port.S4)
 us_sensor = UltrasonicSensor(Port.S3)
 
 execute_program = True
-
 debug_mode = False
 
 RED = 10
 GREEN = 10
-BLUE = 10
+BLUE = 20
 
-turn_rate = 10
-drive_speed = -100
+turn_rate = 60
+drive_speed = 120
 
 ev3 = EV3Brick()
-robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=127)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=82, axle_track=121)
 
 def entertainment(number):
     if number == 1:
-        robot.turn(360)
+       robot.turn(45)
+       robot.turn(-90)
+       robot.turn(45)
     elif number == 2:
         ev3.speaker.say("Never gonna give you up.")
     elif number == 3:
-        robot.turn(360)
+        for i in range(1,10):
+            ev3.light.on(Color.RED)
+            wait(100)
+            ev3.light.on(Color.GREEN)
+            wait(100)
+            ev3.light.on(Color.BLUE)
+            wait(100)
+        ev3.light.off
     else:
-        robot.turn(360)
-    wait(250)
+        ev3.speaker.play_file(SoundFile.DOG_BARK_1)
 
 # TODO Increase turn radius while outside black line
 # Write your program here.
@@ -46,10 +53,12 @@ ev3.speaker.beep()
 start_time = time.time()
 
 if (debug_mode):
-    execute_program = False
-    red, green, blue = color_sensor.rgb()
-    print("Red: {}, Green: {}, Blue: {},".format(red, green, blue))
-    print(str(is_black))
+    while True:
+        execute_program = False
+        red, green, blue = color_sensor.rgb()
+        is_black = red < RED or blue < BLUE or green < GREEN
+        print("Red: {}, Green: {}, Blue: {},".format(red, green, blue))
+        print(str(is_black))
 
 
 while (execute_program):
@@ -57,17 +66,17 @@ while (execute_program):
     is_black = red < RED or blue < BLUE or green < GREEN
 
     if is_black:
-        robot.drive(drive_speed, turn_rate)
+        robot.drive(-drive_speed, -turn_rate)
     else:
-        robot.drive(drive_speed, -turn_rate)
+        robot.drive(-drive_speed, turn_rate)
 
-    if us_sensor.distance() < 200:
+    if us_sensor.distance() < 100:
         robot.stop()
         break
 
     if time.time() - start_time >= 10:
-        start_time = time.time()
         robot.stop()
         entertainment(random.randint(1,4))
+        start_time = time.time()
 
 ev3.speaker.play_file(SoundFile.CHEERING)
