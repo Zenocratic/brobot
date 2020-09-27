b900400deda9ba275857c6064579b57ca8c3cbc8
Erik Borgeteien Hansen <github@borgeteienhansen.no>
2020-09-27 16:45:12 +0200

    calibrate function

--- BroBot.py
+++ BroBot.py
@@ -1,14 +1,53 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
 class BroBot():
     def __init__(self):
         left_motor = Motor(Port.B)
         right_motor = Motor(Port.C)
         self.brobot = DriveBase(left_motor, right_motor, wheel_diameter=55, axle_track=127)
         self.left_color_sensor = ColorSensor(Port.S4)
-        self.right_color_sensor = ColorSensor(Port.S3)
+        self.right_color_sensor = ColorSensor(Port.S2)
 
-    def measure_color(self):
-        return self.left_color_sensor() + self.right_color_sensor()
+        self.ev3 = EV3Brick()
 
-    def is_black(self, thresholds):
-        rgb = self.measure_color()
-        return rgb[0] < thresholds[0] and rgb[1] < thresholds[1] and rgb[2] < thresholds[2] and rgb[3] < thresholds[0] and rgb[4] < thresholds[1] and rgb[5] < thresholds[2]
+        self.RED_ON_WHITE = 57
+        self.RED_ON_BLACK = 5
+
+        self.GREEN_ON_WHITE = 55
+        self.GREEN_ON_BLACK = 4
+
+        self.BLUE_ON_WHITE = 100
+        self.BLUE_ON_BLACK = 10
+
+        self.RED = (self.RED_ON_WHITE + self.RED_ON_BLACK) // 2
+        self.GREEN = (self.GREEN_ON_WHITE + self.GREEN_ON_BLACK) // 2
+        self.BLUE = (self.BLUE_ON_WHITE + self.BLUE_ON_BLACK) // 2
+
+    def calibrate(self):
+        self.ev3.screen.print("Calibrating...\nL-SENSOR: WHITE\nR-SENSOR: BLACK\nPUSH A BUTTON\nTO CONTINUE")
+        while True:
+            if self.ev3.buttons.pressed():
+                break
+            continue
+        rgb = self.left_color_sensor.rgb() + self.right_color_sensor.rgb()
+
+        self.RED_ON_WHITE = rgb[0]
+        self.GREEN_ON_WHITE = rgb[1]
+        self.BLUE_ON_WHITE = rgb[2]
+
+        self.RED_ON_BLACK = rgb[3]
+        self.GREEN_ON_BLACK = rgb[4]
+        self.BLUE_ON_BLACK = rgb[5]
+
+        print("RGB thresholds before: " + str(self.RED) + str(self.GREEN) + str(self.BLUE))
+
+        self.RED = (self.RED_ON_WHITE + self.RED_ON_BLACK) // 2
+        self.GREEN = (self.GREEN_ON_WHITE + self.GREEN_ON_BLACK) // 2
+        self.BLUE = (self.BLUE_ON_WHITE + self.BLUE_ON_BLACK) // 2
+
+        print("RGB thresholds after: " + str(self.RED) + str(self.GREEN) + str(self.BLUE))
--- main.py
+++ main.py
@@ -5,50 +5,9 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
-# Turn on (true) or off (false) calibration mode 
-calibration_mode = False
-
-# Enter measured reflection of RGB-light
-RED_ON_WHITE = 100
-RED_ON_BLACK = 0
-
-GREEN_ON_WHITE = 100
-GREEN_ON_BLACK = 0
-
-BLUE_ON_WHITE = 100
-BLUE_ON_BLACK = 0
-
-# Calculate thresholds
-RED = (RED_ON_WHITE + RED_ON_BLACK) // 2
-GREEN = (GREEN_ON_WHITE + GREEN_ON_BLACK) // 2
-BLUE = (BLUE_ON_WHITE + BLUE_ON_BLACK) // 2
+from BroBot import BroBot
 
 # Initiate EV3 & BroBot
-ev3 = EV3Brick()
-robot = BroBot()
-execute_program = True
+brobot = BroBot()
 
-# Calibration loop: prints measured values every 100ms
-if (calibration_mode):
-    execute_program = False
-    i = 0
-    while True:
-        rgb = robot.measure_color()
-        left_is_black = rgb[0] < RED and rgb[1] < GREEN and rgb[2] < BLUE
-        right_is_black = rgb[3] < RED and rgb[4] < GREEN and rgb[5] < BLUE
-        print("\nLEFT\tR: {0[0]}\tG: {0[1]}\tB: {0[2]}\tBLACK: {1}\nRIGHT\tR: {0[3]}\tG: {0[4]}\tB: {0[5]}\tBLACK: {2}\n".format(rgb, left_is_black, right_is_black))
-        wait(100)
-
-
-turn_rate = 60
-drive_speed = 120
-
-while (execute_program):
-    red, green, blue = color_sensor.rgb()
-    is_black = red < RED or blue < BLUE or green < GREEN
-
-    if is_black:
-        robot.drive(drive_speed, -turn_rate)
-    else:
-        robot.drive(drive_speed, turn_rate)
+brobot.calibrate()
